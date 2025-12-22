fb8fae6c5e86f212304e222d23d1acef076b6e43
Nir Soffer <nirsof@gmail.com>
2025-12-22 06:02:50 +0200

    Improve logging
    Add prefix to all log to keep the context:
    - [peer pid] for code handling events for peer event
    - [main] for main thread logs
    
    Log connected message when client connects.
    
    Example logs:
    
        INFO  [peer 3300] connected
        INFO  [peer 3300] send reply: 1
        INFO  [peer 3300] disconnected
        INFO  [peer 3315] connected
        INFO  [peer 3315] send reply: 2
        INFO  [peer 3315] disconnected
        INFO  [peer 3410] connected
        WARN  [peer 3410] send error: (1) no command
        DEBUG [peer 3410] send error reply: <dictionary: 0x10221d470> { count = 1, transaction: 1, voucher = 0x10221d230, contents =
                "error" => <dictionary: 0xb59400000> { count = 2, transaction: 0, voucher = 0x0, contents =
                        "message" => <string: 0x102216c10> { string cache = 0x0, length = 10, contents = "no command" }
                        "code" => <int64: 0xb2949a30c8cf747f>: 1
                }
        }
        INFO  [peer 3410] disconnected
        INFO  [peer 3516] connected
        WARN  [peer 3516] send error: (1) unknown command
        DEBUG [peer 3516] send error reply: <dictionary: 0x10221d280> { count = 1, transaction: 1, voucher = 0x10221d230, contents =
                "error" => <dictionary: 0xb59400000> { count = 2, transaction: 0, voucher = 0x0, contents =
                        "message" => <string: 0x102216c10> { string cache = 0x0, length = 15, contents = "unknown command" }
                        "code" => <int64: 0xb2949a30c8cf747f>: 1
                }
        }
        INFO  [peer 3516] disconnected

--- broker.c
+++ broker.c
@@ -6,19 +6,24 @@
 #include "vmnet-broker.h"
 #include "log.h"
 
+struct peer {
+    xpc_connection_t connection;
+    pid_t pid;
+};
+
 bool verbose = true;
 
 // Concurrent clients run can access this in parallel.
 static atomic_uint_least64_t global_counter = 0;
 
-static void send_error(xpc_connection_t peer, xpc_object_t event, int code, const char *message) {
-    WARNF("send error: (%d) %s", code, message);
+static void send_error(const struct peer *peer, xpc_object_t event, int code, const char *message) {
+    WARNF("[peer %d] send error: (%d) %s", peer->pid, code, message);
 
     xpc_object_t reply = xpc_dictionary_create_reply(event);
     if (reply == NULL) {
         // Event does not include the return address.
         char *desc = xpc_copy_description(event);
-        WARNF("failed create reply for event: %s", desc);
+        WARNF("[peer %d] failed create reply for event: %s", peer->pid, desc);
         free(desc);
         return;
     }
@@ -26,7 +31,7 @@ static void send_error(xpc_connection_t peer, xpc_object_t event, int code, cons
     xpc_object_t error = xpc_dictionary_create(NULL, NULL, 0);
     if (error == NULL) {
         // Should not happen.
-        ERROR("failed to create error");
+        ERRORF("[peer %d] failed to create error", peer->pid);
         goto out;
     }
 
@@ -38,50 +43,50 @@ static void send_error(xpc_connection_t peer, xpc_object_t event, int code, cons
 
     if (verbose) {
         char *desc = xpc_copy_description(reply);
-        DEBUGF("send error reply: %s", desc);
+        DEBUGF("[peer %d] send error reply: %s", peer->pid, desc);
         free(desc);
     }
 
-    xpc_connection_send_message(peer, reply);
+    xpc_connection_send_message(peer->connection, reply);
 
 out:
     xpc_release(reply);
 }
 
-static void send_reply(xpc_connection_t peer, xpc_object_t event, uint64_t value) {
-    INFOF("send reply: %llu", value);
+static void send_reply(const struct peer *peer, xpc_object_t event, uint64_t value) {
+    INFOF("[peer %d] send reply: %llu", peer->pid, value);
 
     xpc_object_t reply = xpc_dictionary_create_reply(event);
     if (reply == NULL) {
         // Event does not include the return address.
         char *desc = xpc_copy_description(event);
-        WARNF("failed create reply for event: %s", desc);
+        WARNF("[peer %d] failed create reply for event: %s", peer->pid, desc);
         free(desc);
         return;
     }
 
     xpc_dictionary_set_uint64(reply, REPLY_VALUE, value);
-    xpc_connection_send_message(peer, reply);
+    xpc_connection_send_message(peer->connection, reply);
 
     xpc_release(reply);
 }
 
-static void handle_error(xpc_connection_t peer, xpc_object_t event) {
+static void handle_error(const struct peer *peer, xpc_object_t event) {
     if (event == XPC_ERROR_CONNECTION_INVALID) {
         // Client connection is dead - invalidate resources owned by client.
-        INFOF("peer %p has disconnected", (void *)peer);
+        INFOF("[peer %d] disconnected", peer->pid);
     } else if (event == XPC_ERROR_CONNECTION_INTERRUPTED) {
         // Temporary interruption, may recover.
-        INFOF("peer %p temporary interruption", (void *)peer);
+        INFOF("[peer %d] temporary interruption", peer->pid);
     } else {
         // Unexpected error, log all the details.
         char *desc = xpc_copy_description(event);
-        WARNF("peer %p unexpected error: %s", (void *)peer, desc);
+        WARNF("[peer %d] unexpected error: %s", peer->pid, desc);
         free(desc);
     }
 }
 
-static void handle_request(xpc_connection_t peer, xpc_object_t event) {
+static void handle_request(const struct peer *peer, xpc_object_t event) {
     const char* command = xpc_dictionary_get_string(event, REQUEST_COMMAND);
     if (command == NULL) {
         send_error(peer, event, ERROR_INVALID_REQUEST, "no command");
@@ -94,31 +99,42 @@ static void handle_request(xpc_connection_t peer, xpc_object_t event) {
     send_reply(peer, event, ++global_counter);
 }
 
-static void handle_event(xpc_connection_t peer) {
-    // TODO: register the peer and extract the audit token and pid
+static void handle_event(xpc_connection_t connection) {
+    // Until we manage list of active connectons, this is a good place to keep
+    // the peer. It is shared with all requests on this connection via the
+    // handler block. We need to capture now the pid since it is not available
+    // when the connecton invalidates.
+    struct peer peer = {
+        .connection = connection,
+        .pid = xpc_connection_get_pid(connection),
+    };
+
+    INFOF("[peer %d] connected", peer.pid);
+
+    // TODO: register the peer and extract the audit token
     // TODO: authorize the client using the audit token
 
-    xpc_connection_set_event_handler(peer, ^(xpc_object_t event) {
+    xpc_connection_set_event_handler(peer.connection, ^(xpc_object_t event) {
         xpc_type_t type = xpc_get_type(event);
         if (type == XPC_TYPE_ERROR) {
-            handle_error(peer, event);
+            handle_error(&peer, event);
         } else if (type == XPC_TYPE_DICTIONARY) {
-            handle_request(peer, event);
+            handle_request(&peer, event);
         }
     });
 
-    xpc_connection_resume(peer);
+    xpc_connection_resume(connection);
 }
 
 int main() {
     xpc_connection_t listener = xpc_connection_create_mach_service(MACH_SERVICE_NAME, NULL, XPC_CONNECTION_MACH_SERVICE_LISTENER);
     if (!listener) {
-        ERROR("failed to create mach service listener");
+        ERROR("[main] failed to create mach service listener");
         exit(EXIT_FAILURE);
     }
 
-    xpc_connection_set_event_handler(listener, ^(xpc_object_t peer) {
-        handle_event(peer);
+    xpc_connection_set_event_handler(listener, ^(xpc_object_t connection) {
+        handle_event(connection);
     });
 
     xpc_connection_resume(listener);
