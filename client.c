0d2955a2717cba541ee994703758a0a35fc016ec
Nir Soffer <nirsof@gmail.com>
2025-12-23 02:15:58 +0200

    Improve client to start a vment interface
    We want to test that the network we got from the broker can be used, and
    show the network details. The best way to do this in a simple client is
    to stat the vment interface with the network. The completion callback
    includes a param xpc_dictionary_t with the network details.
    
    The disadvantage is requiring root to run the client. In a real
    application the user will start a vm using the virtualization framework
    which has the permission to stat the vment interface.
    
    Example run:
    
        % sudo ./vmnet-client
        INFO  connecting to broker
        INFO  requesting network
        INFO  network recived
        DEBUG starting vmnet interface from network
        INFO  vmnet interface started
        {"vmnet_mtu":1500,"vmnet_write_max_packets":256,"vmnet_read_max_packets":256,"vmnet_interface_id":"D7B57194-F3FF-49FE-A8C8-CE4B7FDDA7A5","vmnet_max_packet_size":1514,"vmnet_mac_address":"52:37:24:27:68:12"}
        INFO  waitng for termination

--- client.c
+++ client.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
+#include <time.h>
+#include <vmnet/vmnet.h>
 #include <xpc/xpc.h>
+#include <uuid/uuid.h>
 
 #include "vmnet-broker.h"
 #include "log.h"
@@ -9,6 +12,9 @@ bool verbose = true;
 // The connection must be kept open during the lifetime of the client. The
 // kernel invalidates the broker connection after the client terminates.
 static xpc_connection_t connection;
+
+// Client starts a vmnet interface using the network returned by the broker.
+static interface_ref interface;
 
 static void connect_to_broker(void) {
     INFO("connecting to broker");
@@ -28,16 +34,16 @@ static void connect_to_broker(void) {
 }
 
 // Get a network from the broker.
-static vmnet_network_ref request_network(void) {
+static vmnet_network_ref request_network_from_broker(void) {
     INFO("requesting network");
-
-    vmnet_network_ref network = NULL;
 
     xpc_object_t message = xpc_dictionary_create_empty();
     xpc_dictionary_set_string(message, REQUEST_COMMAND, COMMAND_GET);
 
     xpc_object_t reply = xpc_connection_send_message_with_reply_sync(connection, message);
     xpc_clear(&message);
+
+    vmnet_network_ref network = NULL;
 
     if (xpc_get_type(reply) == XPC_TYPE_ERROR) {
         char *desc = xpc_copy_description(reply);
@@ -76,20 +82,89 @@ static vmnet_network_ref request_network(void) {
         goto out;
     }
 
+    INFO("network received");
+
 out:
     xpc_release(reply);
     return network;
 }
 
+static void write_vmnet_info(xpc_object_t param)
+{
+    __block int count = 0;
+
+#define print_item(fmt, key, value) \
+    do { \
+        if (count++ > 0) \
+            printf(","); \
+        printf(fmt, key, value); \
+    } while (0)
+
+    printf("{");
+
+    xpc_dictionary_apply(param, ^bool(const char *key, xpc_object_t value) {
+        xpc_type_t t = xpc_get_type(value);
+        if (t == XPC_TYPE_UINT64) {
+            print_item("\"%s\":%llu", key, xpc_uint64_get_value(value));
+        } else if (t == XPC_TYPE_INT64) {
+            print_item("\"%s\":%lld", key, xpc_int64_get_value(value));
+        } else if (t == XPC_TYPE_STRING) {
+            print_item("\"%s\":\"%s\"", key, xpc_string_get_string_ptr(value));
+        } else if (t == XPC_TYPE_UUID) {
+            char uuid_str[36 + 1];
+            uuid_unparse(xpc_uuid_get_bytes(value), uuid_str);
+            print_item("\"%s\":\"%s\"", key, uuid_str);
+        }
+        return true;
+    });
+
+    printf("}\n");
+    fflush(stdout);
+}
+
+static void start_interface_from_network(vmnet_network_ref network) {
+    DEBUG("starting vmnet interface from network");
+
+    xpc_object_t desc = xpc_dictionary_create_empty();
+    dispatch_queue_t queue = dispatch_queue_create("com.github.nirs.vmnet-client", DISPATCH_QUEUE_SERIAL);
+    dispatch_semaphore_t completed = dispatch_semaphore_create(0);
+
+    interface = vmnet_interface_start_with_network(network, desc, queue, ^(vmnet_return_t status, xpc_object_t param){
+        if (status != VMNET_SUCCESS) {
+            ERRORF("failed to start vment interface with network: (%d) %s", status, vmnet_strerror(status));
+            exit(EXIT_FAILURE);
+        }
+
+        INFO("vmnet interface started");
+        write_vmnet_info(param);
+        dispatch_semaphore_signal(completed);
+    });
+
+    dispatch_semaphore_wait(completed, DISPATCH_TIME_FOREVER);
+
+    dispatch_release(completed);
+    dispatch_release(queue);
+    xpc_release(desc);
+}
+
 int main(int argc, char *argv[]) {
     connect_to_broker();
 
-    vmnet_network_ref network = request_network();
+    vmnet_network_ref network = request_network_from_broker();
     if (network == NULL) {
         exit(EXIT_FAILURE);
     }
 
-    INFO("network received");
-    CFRelease(network);
+    // NOTE: This requires root. Real client will create a virtual machine with
+    // a VZVmnetNetworkDeviceAttachment, which does not require root or the
+    // "com.apple.networking" entitlement. The virtualization framework service
+    // for the vm have the privileges to start the vment interface.
+    // https://developer.apple.com/documentation/virtualization/vzvmnetnetworkdeviceattachment?language=objc
+    start_interface_from_network(network);
+
+    // Simulate starting a VM. The connection is alive while the process is running.
+    INFO("waiting for termination");
+    sleep(300);
+
     return 0;
 }
