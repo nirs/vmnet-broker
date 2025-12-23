89998050b2f970dcd0444ad0ab6eb269da30d4c3
Nir Soffer <nirsof@gmail.com>
2025-12-23 06:01:24 +0200

    Fix usage of xpc_clear
    It must accept a pointer to xpc_object_t. The issue is that xpc_object_t
    contains pointers to other xpc_object_t, so wrong call is not detected
    by the compiler.

--- broker.c
+++ broker.c
@@ -103,7 +103,7 @@ static void send_error(const struct peer *peer, xpc_object_t event, int code, co
     xpc_dictionary_set_string(error, ERROR_MESSAGE, message);
 
     xpc_dictionary_set_value(reply, REPLY_ERROR, error);
-    xpc_clear(error);
+    xpc_clear(&error);
 
     if (verbose) {
         char *desc = xpc_copy_description(reply);
